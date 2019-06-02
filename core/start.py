c57f5c8b7e2dda8310bdfe4638f6d0ae2d239b05
Albert <fltermare@gmail.com>
2019-06-02 21:59:22 +0800

    build dag

--- core/start.py
+++ core/start.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
-
+from cmd import Cmd
 from flask_server.app import run_server
 from core.db import db_init, dump2db
 from core.airflow_cmd import set_airflow_env, init_airflow_db, start_airflow
 from core.my_utils import get_opt
+
+
+class MyPrompt(Cmd):
+
+    prompt = "(Stock) "
+
+    def do_hello(self, line):
+        print('hello')
+
+    def do_EOF(self, line):
+        return True
 
 
 def entry():
@@ -18,3 +29,5 @@ def entry():
 
     if args.start:
         run_server()
+
+    #MyPrompt().cmdloop()
