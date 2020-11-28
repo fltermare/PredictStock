871ea97d39ed7f32c00f068c25a3521e5cf86122
Albert <fltermare@gmail.com>
2020-11-28 19:13:39 +0800

    update management page

--- webserver/core/controller/view.py
+++ webserver/core/controller/view.py
@@ -5,26 +5,25 @@ from core.model.database import get_available_stock_info, add_new_stock, delete_
 view_page = Blueprint('view_page', __name__, template_folder='templates')
 
 
-# @view_page.route('/3')
-# def auths():
-#     res = query_stock_name()
-#     return 'auth3' + "[%s]" % res
+# @view_page.route('/get_test', methods=['GET'])
+# def get_test():
+#     name = request.values.get('name')
+#     return 'GET: ' + str(name)
 
-# @view_page.route('/1')
-# def db():
-#     # res = query_available_stock()
-#     res = history_price()
-#     return 'db'
 
-# @view_page.route('/2')
-# def db2():
-#     res = get_stock_list()
-#     print(res)
-#     print(get_options(res))
-#     df_sub = history_price(get_stock_list())
-#     print(df_sub)
-#     print(sorted(get_stock_list())[0])
-#     return str(res)
+# @view_page.route('/post_test', methods=['POST'])
+# def post_test():
+#     var = request.values.get('var')
+#     return 'POST: ' + str(var)
+
+
+@view_page.route('/predict/<string:stock_code>', methods=['POST'])
+def predict(stock_code):
+    # NOT restful api
+    # todo
+    # check stock in DB
+    # display price and prediction
+    return stock_code
 
 
 @view_page.route('/dashboard')
@@ -38,8 +37,9 @@ def manage():
 
     if request.method == "POST":
         print('--------------')
-        add_new_stock(request.form['stock_code'])
-        flash('[Added] Downloading Now', 'success')
+        stock_code = request.form['stock_code']
+        add_new_stock(stock_code)
+        flash("[Added] Downloading {} Now".format(stock_code), 'success')
         print('--------------')
 
     stock_info = get_available_stock_info()
@@ -47,15 +47,17 @@ def manage():
     return render_template('manage.html', stock_info=stock_info)
 
 
+@view_page.route("/remove", methods=["POST"])
+def remove():
 
-@view_page.route("/remove/<string:stock_code>", methods=["POST"])
-def remove(stock_code):
+    stock_code = str(request.values.get('stock_code'))
+    # method = request.values.get('_method')
 
     try:
         delete_stock(stock_code)
-        flash('Delete', 'success')
+        flash("Delete {}".format(stock_code), 'success')
     except Exception as err:
-        flash('Delete Failed' + err, 'danger')
+        flash("Delete {} Failed".format(stock_code) + err, 'danger')
 
     return redirect(url_for('view_page.manage'))
 
